5832a6852e1172f9d9bbfcd92ad3540f74b3a175
rishab18bisht <124886904+rishab18bisht@users.noreply.github.com>
2023-02-10 12:25:09 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,35 @@
 
 
 def call() {
-	sh "echo hello world"
+---
+  name: Install Jenkins full package and installation as well
+- hosts: ansible
+  become: yes
+  tasks:
+   - name: download jenkins repo
+     apt_key:
+       url: https://pkg.jenkins.io/debian-stable/jenkins.io.key
+       state: present
+   - name: repository is configured
+     apt_repository:
+       repo: deb https://pkg.jenkins.io/debian-stable binary/
+       state: present
+   - name: apt update cache
+     apt:
+       update_cache: yes
+       state: latest
+   - name: Install Java
+     apt:
+       name: openjdk-11-jdk
+       state: present
+   - name: jenkins
+     apt:
+       name: jenkins
+       state: latest
+   - name: start jenkins
+     service:
+       name: jenkins
+       enabled: true
+       state: started
+...
 	}
